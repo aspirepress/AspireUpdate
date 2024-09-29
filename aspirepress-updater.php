0abf5c664ffc2b3f5cac730e651c41e2c2f06cc2
Sarah Savage <me@sarah-savage.com>
2024-09-29 08:22:18 -0400

    Adding API key capabilities to the AspirePress plugin

--- aspirepress-updater.php
+++ aspirepress-updater.php
@@ -22,10 +22,6 @@ function aspirePressAutoloader($class)
     return false;
 }
 
-if (! defined('AP_UPDATER_API_KEY')) {
-    define('AP_UPDATER_API_KEY', false);
-}
-
 if ( defined('AP_UPDATER_DEBUG') && AP_UPDATER_DEBUG ) {
     AspirePress_Debug::enableDebug();
 }
@@ -68,35 +64,69 @@ if (! defined('AP_UPDATER_EXAMINE_RESPONSES')) {
     define('AP_UPDATER_EXAMINE_RESPONSES', false);
 }
 
-$aspirePressUpdater = new AspirePress_Updater(
-    new AspirePress_RewriteUrls($rewriteRuleDefs),
-    new AspirePress_HeaderManager(WP_SITEURL, AP_UPDATER_API_KEY)
-);
+if ($apiKey = get_option('wp_api_key')) {
 
-add_filter('pre_http_request', function (...$args) use ($aspirePressUpdater) {
-    $arguments = $args[1] ?? [];
-    $url = $args[2] ?? null;
+    $aspirePressUpdater = new AspirePress_Updater(
+        new AspirePress_RewriteUrls($rewriteRuleDefs),
+        new AspirePress_HeaderManager(WP_SITEURL, $apiKey)
+    );
 
-    if (!$url) {
-        return false;
-    }
+    add_filter('pre_http_request', function (...$args) use ($aspirePressUpdater) {
+        $arguments = $args[1] ?? [];
+        $url = $args[2] ?? null;
 
-    if (AP_UPDATER_DEBUG_SSL) {
-        $arguments['sslverify'] = false;
-    }
-
-    return $aspirePressUpdater->callApi($url, $arguments);
-}, 100, 3);
-
-if (AP_UPDATER_EXAMINE_RESPONSES) {
-    add_filter('http_api_debug', function (...$args) use ($aspirePressUpdater) {
-        $response = $args[0];
-        $url = $args[4];
-
-        if (empty($response) || empty($url)) {
-            return $response;
+        if (!$url) {
+            return false;
         }
-        $aspirePressUpdater->examineResponse($url, $response);
-        return $response;
-    }, 10, 5);
+
+        if (AP_UPDATER_DEBUG_SSL) {
+            $arguments['sslverify'] = false;
+        }
+
+        return $aspirePressUpdater->callApi($url, $arguments);
+    }, 100, 3);
+
+    if (AP_UPDATER_EXAMINE_RESPONSES) {
+        add_filter('http_api_debug', function (...$args) use ($aspirePressUpdater) {
+            $response = $args[0];
+            $url = $args[4];
+
+            if (empty($response) || empty($url)) {
+                return $response;
+            }
+            $aspirePressUpdater->examineResponse($url, $response);
+            return $response;
+        }, 10, 5);
+    }
+
 }
+
+function api_key_setting()
+{
+    $value = get_option('ap_api_key');
+    echo '<input id="ap_api_key" name="ap_api_key" value="' . $value . '" type="password" />';
+}
+
+function ap_text()
+{
+    echo 'Enter your API key for AspirePress. <strong>Note: You cannot access AspirePress until you have entered an API key.</strong><br /><br /><a href="#">Get your API key here.</a>';
+}
+
+add_action('admin_init', function () {
+    add_settings_section('ap_api_key_section', 'AspirePress API Key', 'ap_text', 'general');
+    register_setting('general', 'ap_api_key');
+    add_settings_field( 'ap_api_key', 'AspirePress API Key', 'api_key_setting', 'general', 'ap_api_key_section');
+});
+
+if (! $apiKey) {
+    function ap_admin_notice_no_api_key() {
+        $class = 'notice notice-warning';
+        $message = 'You have not defined an API key for AspirePress. You cannot use AspirePress until you have defined an API key.';
+        $link = '<a href="#">Get one now.</a>';
+
+        printf( '<div class="%1$s"><p>%2$s %3$s</p></div>', esc_attr( $class ), esc_html( $message ), $link );
+    }
+    add_action( 'admin_notices', 'ap_admin_notice_no_api_key' );
+}
+
+
