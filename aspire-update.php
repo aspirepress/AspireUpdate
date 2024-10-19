ad0f03a4ae757abd0c3cd810077cffb64fccf67d
Alex Sirota <alex@newpathconsulting.com>
2024-10-19 09:15:07 -0400

    update php minimum requirement
    as per issue #30  to make consistent with the README.md
    
    Signed-off-by: Alex Sirota <alex@newpathconsulting.com>

--- aspire-update.php
+++ aspire-update.php
@@ -14,7 +14,7 @@
  * Author:            AspirePress
  * Author URI:        https://docs.aspirepress.org/aspireupdate/
  * Requires at least: 4.0
- * Requires PHP:      8.0.0
+ * Requires PHP:      7.4.0
  * Tested up to:      6.7
  * License:           GPL-3.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.txt
