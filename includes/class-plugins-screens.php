98d3d549242a429e2e22058b68aba1d3f346aa60
costdev <costdev@git.wordpress.org>
2024-10-31 18:08:02 +0000

    Remove unused tabs when API rewrite is enabled.

--- includes/class-plugins-screens.php
+++ includes/class-plugins-screens.php
@@ -19,9 +19,23 @@ class Plugins_Screens {
 	private static $instance = null;
 
 	/**
+	 * Hold an array of unsupported filters.
+	 *
+	 * @var array
+	 */
+	protected $unsupported_filters = array(
+		'featured',
+		'favorites',
+	);
+
+	/**
 	 * The Constructor.
 	 */
 	public function __construct() {
+		$admin_settings = Admin_Settings::get_instance();
+		if ( $admin_settings->get_setting( 'enable', false ) ) {
+			add_filter( 'install_plugins_tabs', array( $this, 'remove_unused_filter_tabs' ) );
+		}
 	}
 
 	/**
@@ -34,5 +48,18 @@ class Plugins_Screens {
 			self::$instance = new self();
 		}
 		return self::$instance;
+	}
+
+	/**
+	 * Remove unused filter tabs from the Add New Plugin screen.
+	 *
+	 * @param array $tabs An array of tab labels, keyed on each tab's slug.
+	 * @return array An array of tabs.
+	 */
+	public function remove_unused_filter_tabs( $tabs ) {
+		foreach ( $this->unsupported_filters as $filter ) {
+			unset( $tabs[ $filter ] );
+		}
+		return $tabs;
 	}
 }
