2a0745aa165e56b906a8ddc91bce4d95e94765a4
harikrishnanr994 <harikrishnanr994@gmail.com>
2024-11-04 01:46:23 +0100

    updated German translation

--- languages/AspireUpdate-de_DE.I10n.php
+++ languages/AspireUpdate-de_DE.I10n.php
@@ -1,5 +1,5 @@
 <?php
-return ['project-id-version'=>'AspireUpdate','report-msgid-bugs-to'=>'','pot-creation-date'=>'2024-11-03 17:59+0000','po-revision-date'=>'2024-11-03 18:06+0000','last-translator'=>'','language-team'=>'German (Germany)','language'=>'de_DE','plural-forms'=>'nplurals=2; plural=n != 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.6.11; wp-6.6.2','x-domain'=>'AspireUpdate','messages'=>[
+return ['project-id-version'=>'AspireUpdate','report-msgid-bugs-to'=>'','pot-creation-date'=>'2024-11-04 00:29+0000','po-revision-date'=>'2024-11-04 00:29+0000','last-translator'=>'','language-team'=>'German (Germany)','language'=>'de_DE','plural-forms'=>'nplurals=2; plural=n != 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.6.11; wp-6.6.2','x-domain'=>'AspireUpdate','messages'=>[
     'API Configuration'=>'API-Konfiguration',
     'API Debug Configuration'=>'API-Debug-Konfiguration',
     'API Host'=>'API-Host',
