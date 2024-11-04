23c6811d59263b8a1e58b078a8cfbd1124185d7e
harikrishnanr994 <harikrishnanr994@gmail.com>
2024-11-04 01:36:51 +0100

    added German translation

---
+++ languages/AspireUpdate-de_DE.I10n.php
@@ -0,0 +1,25 @@
+<?php
+return ['project-id-version'=>'AspireUpdate','report-msgid-bugs-to'=>'','pot-creation-date'=>'2024-11-03 17:59+0000','po-revision-date'=>'2024-11-03 18:06+0000','last-translator'=>'','language-team'=>'German (Germany)','language'=>'de_DE','plural-forms'=>'nplurals=2; plural=n != 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.6.11; wp-6.6.2','x-domain'=>'AspireUpdate','messages'=>[
+    'API Configuration'=>'API-Konfiguration',
+    'API Debug Configuration'=>'API-Debug-Konfiguration',
+    'API Host'=>'API-Host',
+    'API Key'=>'API-Schlüssel',
+    'AspirePress'=>'AspirePress',
+    'AspireUpdate'=>'AspireUpdate',
+    'AspireUpdate Settings'=>'AspireUpdate-Einstellungen',
+    'Disable SSL Verification'=>'SSL-Überprüfung deaktivieren',
+    'Disables the verification of SSL to allow local testing.'=>'Deaktiviert die SSL-Überprüfung, um lokale Tests zu ermöglichen.',
+    'Enable AspireUpdate API Rewrites'=>'Aktivieren der AspireUpdate API-Umschreibungen',
+    'Enable Debug Mode'=>'Debug-Modus aktivieren',
+    'Enable Debug Type'=>'Debug-Typ aktivieren',
+    'Enables debug mode for the plugin.'=>'Aktiviert den Debug-Modus für das Plugin.',
+    'https://aspirepress.org/'=>'AspireUpdate',
+    'https://docs.aspirepress.org/aspireupdate/'=>'https://docs.aspirepress.org/aspireupdate/',
+    'Outputs the request URL and headers / response headers and body / string that is being rewritten.'=>'Gibt die Anfrage-URL und Header / Antwort-Header und -Body / Zeichenfolge aus, die umgeschrieben wird.',
+    'Provides an API key for repositories that may require authentication.'=>'Stellt einen API-Schlüssel für Repositories bereit, die möglicherweise eine Authentifizierung erfordern.',
+    'Request'=>'Anfrage',
+    'Response'=>'Antwort',
+    'String'=>'Zeichenkette',
+    'Update plugins and themes for WordPress.'=>'Aktualisiert Plugins und Themes für WordPress.',
+    'Your new API Host.'=>'Ihr neuer API-Host'
+]];
