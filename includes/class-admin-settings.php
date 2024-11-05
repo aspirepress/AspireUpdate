ee159a0c99bbfd8aae3125db031c6580d395a00b
costdev <costdev@git.wordpress.org>
2024-11-05 13:49:35 +0000

    Ensure `$_POST['aspireupdate_settings']` is set.

--- includes/class-admin-settings.php
+++ includes/class-admin-settings.php
@@ -239,8 +239,7 @@ class Admin_Settings {
 		}
 
 		// Save settings and redirect.
-		if ( ( isset( $_POST['option_page'] ) && 'aspireupdate_settings' === $_POST['option_page'] ) ) {
-			update_site_option( $this->option_name, $this->sanitize_settings( wp_unslash( $_POST['aspireupdate_settings'] ) ) );
+		if ( ( isset( $_POST['option_page'], $_POST['aspireupdate_settings'] ) && 'aspireupdate_settings' === $_POST['option_page'] ) ) {
 
 			wp_safe_redirect(
 				add_query_arg( [ network_admin_url( 'index.php?page=aspireupdate-settings' ) ] )
