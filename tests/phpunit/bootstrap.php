4d472ec0741b527778cb62d3a757bcbaa817edab
costdev <costdev@git.wordpress.org>
2024-11-13 00:18:32 +0000

    Move test files into `tests/phpunit/`.

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -32,7 +32,7 @@ require_once "{$_tests_dir}/includes/functions.php";
  */
 function _manually_load_plugin() {
 
-	require dirname( __DIR__ ) . '/aspire-update.php';
+	require dirname( __DIR__ ) . '/../aspire-update.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
