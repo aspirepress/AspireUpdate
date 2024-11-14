d65f3298b719b82252d029f6f60db3527893519d
Andy Fragen <andy@thefragens.com>
2024-11-14 15:50:34 -0800

    update referenced to mixed case repository (#193)

--- aspire-update.php
+++ aspire-update.php
@@ -20,7 +20,7 @@
  * License URI:       https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
  * Text Domain:       aspireupdate
  * Domain Path:       /languages
- * GitHub Plugin URI: https://github.com/aspirepress/AspireUpdate
+ * GitHub Plugin URI: https://github.com/aspirepress/aspireupdate
  * Primary Branch:    main
  */
 
