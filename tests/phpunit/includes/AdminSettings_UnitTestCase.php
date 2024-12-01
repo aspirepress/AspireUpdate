2ae4c24db5168850d83a1422b401036c17f9cb2b
Colin Stewart <79332690+costdev@users.noreply.github.com>
2024-12-01 10:03:40 +0000

    Introduce and implement `AdminSettings_UnitTestCase` (#213)

---
+++ tests/phpunit/includes/AdminSettings_UnitTestCase.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Abstract base test class for \AspireUpdate\Admin_Settings.
+ *
+ * All \AspireUpdate\Admin_Settings unit tests should inherit from this class.
+ */
+abstract class AdminSettings_UnitTestCase extends WP_UnitTestCase {
+	/**
+	 * The Name of the Option.
+	 *
+	 * @var string
+	 */
+	protected static $option_name = 'aspireupdate_settings';
+
+	/**
+	 * Deletes settings before each test runs.
+	 *
+	 * @return void
+	 */
+	public function set_up() {
+		parent::set_up();
+
+		delete_site_option( self::$option_name );
+	}
+}
